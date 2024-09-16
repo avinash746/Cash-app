92b4313339f574cdddef03d3983f68819d6e3913
avinash746 <anish95380@gmail.com>
2024-09-17 00:39:05 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import MainLayer from "./Components/MainLayer/index";
+import Payments from './Components/Payments';
+
+import './App.css';
+
+function App() {
+  return (
+    <main>
+       <MainLayer/>
+       <Payments/>
+       <section className="section3">
+       </section>
+    </main>
+  );
+}
+
+export default App;
